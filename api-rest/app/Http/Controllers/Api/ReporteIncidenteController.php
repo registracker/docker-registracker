a49cd6b40a9e48422cb4df23288bba26ec5895df
Juan Carlos Orellana Orellana <juan.orellanao@salud.gob.sv>
2023-03-02 21:31:54 -0600

    fix: cast fecha reporte

--- api-rest/app/Http/Controllers/Api/ReporteIncidenteController.php
+++ api-rest/app/Http/Controllers/Api/ReporteIncidenteController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\ReporteIncidente;
 use Orion\Http\Controllers\Controller;
+use Orion\Http\Requests\Request;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class ReporteIncidenteController extends Controller
 {
     protected $model = ReporteIncidente::class;
+    protected function performStore(Request $request, Model $reporteIncidente, array $attributes): void
+    {
+        $attributes['fecha_reporte'] = Carbon::createFromTimestampMs($attributes['fecha_reporte']);
+        $reporteIncidente->fill($attributes);
+        $reporteIncidente->save();
+    }
 }
