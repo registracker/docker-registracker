9a4ee6ac9e92b1c5a6d26594f09f60707dfa5c33
Juan Carlos Orellana Orellana <juan.orellanao@salud.gob.sv>
2023-03-05 04:46:33 -0600

    fix: typo mdi motorbike

--- api-rest/database/seeders/MedioDesplazamientoSeeder.php
+++ api-rest/database/seeders/MedioDesplazamientoSeeder.php
@@ -54,7 +54,7 @@ class MedioDesplazamientoSeeder extends Seeder
             [
                 'id' => 8,
                 'nombre' => 'Motocicleta',
-                'icono' => 'mdi-motorbike',
+                'icono' => 'motorbike',
             ],
         ];
 
