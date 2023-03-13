a9c749e6f07717ae3aba680e498c5c2f23fd570f
Juan Carlos Orellana Orellana <juan.orellanao@salud.gob.sv>
2023-03-13 13:59:30 -0600

    fix: actualizar y eliminar permiso

--- api-rest/app/Http/Controllers/Api/PermissionController.php
+++ api-rest/app/Http/Controllers/Api/PermissionController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\PermissionRequest;
+use Illuminate\Database\Eloquent\Model;
 use Orion\Http\Controllers\Controller;
+use Orion\Http\Requests\Request;
 use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Arr;
 
 class PermissionController extends Controller
 {
     protected $model = Permission::class;
+
+    protected $request = PermissionRequest::class;
+
+    private $defaultPermisos = 88;
+
     public function limit(): int
     {
         return 100;
@@ -15,6 +24,25 @@ class PermissionController extends Controller
 
     public function filterableBy(): array
     {
-        return ['name'];
+        return ['id', 'name'];
+    }
+    public function sortableBy(): array
+    {
+        return ['id', 'name'];
+    }
+
+    protected function performUpdate(Request $request, Model $permission, array $attributes): void
+    {
+        if ($permission->id > $this->defaultPermisos) {
+            $permission->fill(Arr::only($attributes, ['name']));
+            $permission->save();
+        }
+    }
+
+    protected function performDestroy(Model $permission): void
+    {
+        if ($permission->id > $this->defaultPermisos) {
+            $permission->delete();
+        }
     }
 }
