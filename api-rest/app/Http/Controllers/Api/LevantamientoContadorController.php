bfefdc1a5db95b6d317acb604f4897dd9dda974d
kernel503 <juanko682@gmail.com>
2023-05-07 04:57:24 +0000

    fix: user id contador

--- api-rest/app/Http/Controllers/Api/LevantamientoContadorController.php
+++ api-rest/app/Http/Controllers/Api/LevantamientoContadorController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\LevantamientoContador;
+use Database\Seeders\Constant;
 use Orion\Http\Controllers\Controller;
+use Orion\Http\Requests\Request;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class LevantamientoContadorController extends Controller
 {
@@ -12,5 +17,28 @@ class LevantamientoContadorController extends Controller
     public function includes(): array
     {
         return ['usuario'];
+    }
+
+    protected function keyName(): string
+    {
+        return 'codigo';
+    }
+
+    protected function buildIndexFetchQuery(Request $request, array $requestedRelations): Builder
+    {
+        $query = parent::buildIndexFetchQuery($request, $requestedRelations);
+        if (!$this->resolveUser()->hasRole(Constant::ROL_ADMINISTRADOR)) {
+            $query->where('id_usuario', $this->resolveUser()->id);
+        }
+        return $query;
+    }
+
+    protected function performStore(Request $request, Model $levantaminto, array $attributes): void
+    {
+        $levantaminto->fill(array_merge($attributes, [
+            'id_usuario' => $this->resolveUser()->id,
+            'codigo' => Str::substr(Str::ulid()->toRfc4122(), 9, -13)
+        ]));
+        $levantaminto->save();
     }
 }
