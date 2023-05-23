b500411a7746fe10eb3dac105dba113aa21298ac
kernel503 <juanko682@gmail.com>
2023-05-23 21:47:11 +0000

    feat: headers

--- app/Exports/ReporteContadorExport.php
+++ app/Exports/ReporteContadorExport.php
@@ -4,14 +4,24 @@ namespace App\Exports;
 
 use App\Models\ReporteContador;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ReporteContadorExport implements FromCollection
+class ReporteContadorExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return ReporteContador::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            '#',
+            'User',
+            'Date',
+        ];
     }
 }
