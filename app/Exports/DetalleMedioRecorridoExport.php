d9052298213162354c22378703565074662258bc
Daniel Cortez <francisco.cortez@salud.gob.sv>
2023-05-25 00:59:31 -0600

    filtro recorrido multulpe

--- app/Exports/DetalleMedioRecorridoExport.php
+++ app/Exports/DetalleMedioRecorridoExport.php
@@ -5,19 +5,38 @@ namespace App\Exports;
 use App\Models\DetalleMedioRecorrido;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Support\Facades\DB;
 
 class DetalleMedioRecorridoExport implements FromCollection, WithHeadings
 {
+    protected $fechaInicio;
+    protected $fechaFin;
+
+    public function __construct(string $fechaInicio, string $fechaFin)
+    {
+        $this->fechaInicio = $fechaInicio;
+        $this->fechaFin = $fechaFin;
+    }
+
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
-    {
+    {   
         //return DetalleMedioRecorrido::all();
-        return DetalleMedioRecorrido::join('medios_desplazamiento', 'detalle_medios_recorrido.id_medio_desplazamiento', '=', 'medios_desplazamiento.id')
-        ->select('detalle_medios_recorrido.id', 'detalle_medios_recorrido.desplazamiento_id', 'medios_desplazamiento.nombre','detalle_medios_recorrido.duracion','detalle_medios_recorrido.fecha_creado')
-        ->get();
-    }
+        //return DetalleMedioRecorrido::join('medios_desplazamiento', 'detalle_medios_recorrido.id_medio_desplazamiento', '=', 'medios_desplazamiento.id')
+       // ->select('detalle_medios_recorrido.id', 'detalle_medios_recorrido.desplazamiento_id', 'medios_desplazamiento.nombre','detalle_medios_recorrido.duracion','detalle_medios_recorrido.fecha_creado')
+        //->whereBetween('DATE(detalle_medios_recorrido.fecha_creado)',[$this->fechaInicio , $this->fechaFin])
+        //->get();
+
+        return DB::table('detalle_medios_recorrido  as mr')
+            ->join('medios_desplazamiento', 'mr.id_medio_desplazamiento', '=', 'medios_desplazamiento.id')
+            ->select('mr.id', 'mr.desplazamiento_id', 'medios_desplazamiento.nombre','mr.duracion','mr.fecha_creado')
+            ->whereDate('mr.fecha_creado','>=',$this->fechaInicio)
+            ->whereDate('mr.fecha_creado','<=',$this->fechaFin)
+            ->get();
+        }
+        //->whereBetween('DATE(mr.fecha_creado)', [$this->fechaInicio, $this->fechaFin])
 
     public function headings(): array
     {
