7ed872d2ad82d6b592eb659726c9748fb3a009a1
kernel503 <juanko682@gmail.com>
2023-06-18 06:47:39 +0000

    fix: swagger base url

--- swagger/dist/docs/swagger-initializer.js
+++ swagger/dist/docs/swagger-initializer.js
@@ -2,8 +2,9 @@ window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  
   window.ui = SwaggerUIBundle({
-    url: "https://registracker.me/storage/specs/specs.json",
+    url: window.location.origin+"/storage/specs/specs.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
