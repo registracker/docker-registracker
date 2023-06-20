567698bf17ca2f37d53ff5d6db8ef79a6a45f3c2
kernel503 <juanko682@gmail.com>
2023-06-20 21:07:59 +0000

    fix: permisos

--- database/seeders/RolPermissionSeeder.php
+++ database/seeders/RolPermissionSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Str;
+
 
 class RolPermissionSeeder extends Seeder
 {
@@ -16,40 +18,147 @@ class RolPermissionSeeder extends Seeder
      */
     public function run()
     {
-        $separador = Constant::SEPARADOR;
-        $guardApi = Constant::GUARD_API;
-        $guardWeb = Constant::GUARD_WEB;
-
-        $accionesDisponibles = collect(['crear', 'listar', 'actualizar', 'eliminar']);
-
+        /** 
+         * 100 Administrador
+         * 110 Administrador e investigador
+         * 101 Administrador y usuario
+         * 111 Administrador, investigador y participante
+         */
         $modelos = collect([
-            'zona',
-            'departamento',
-            'municipio',
-            'genero',
-            'universidad',
-            'rol',
-            'incidente',
-            'marcador',
-            'medio_desplazamiento',
-            'permiso',
-            'vehiculo',
-            'estado_solicitud',
-            'user',
-            'desplazamiento',
-            'solicitud_cuenta',
-            'reporte_incidente',
-            'detalle_medio_recorrido',
-            'tipos_vehiculos_rutas',
-            'tipos_servicios_rutas',
-            'clases_servicios_rutas',
-            'rutas_transportes',
-            'levantamiento',
-            'reporte_marcadores',
-            'bitacora_tablas',
-            'levantamiento_contador',
-            'reporte_contador',
-            'terminos_condiciones',
+            'api:zona:crear',
+            'api:zona:listar',
+            'api:zona:actualizar',
+            'api:zona:eliminar',
+
+            'api:departamento:crear',
+            'api:departamento:listar',
+            'api:departamento:actualizar',
+            'api:departamento:eliminar',
+
+            'api:municipio:crear',
+            'api:municipio:listar',
+            'api:municipio:actualizar',
+            'api:municipio:eliminar',
+
+            'api:genero:crear',
+            'api:genero:listar',
+            'api:genero:actualizar',
+            'api:genero:eliminar',
+
+            'api:universidad:crear',
+            'api:universidad:listar',
+            'api:universidad:actualizar',
+            'api:universidad:eliminar',
+
+            'api:rol:crear',
+            'api:rol:listar',
+            'api:rol:actualizar',
+            'api:rol:eliminar',
+
+            'api:incidente:crear',
+            'api:incidente:listar',
+            'api:incidente:actualizar',
+            'api:incidente:eliminar',
+
+            'api:marcador:crear',
+            'api:marcador:listar',
+            'api:marcador:actualizar',
+            'api:marcador:eliminar',
+
+            'api:medio_desplazamiento:crear',
+            'api:medio_desplazamiento:listar',
+            'api:medio_desplazamiento:actualizar',
+            'api:medio_desplazamiento:eliminar',
+
+            'api:permiso:crear',
+            'api:permiso:listar',
+            'api:permiso:actualizar',
+            'api:permiso:eliminar',
+
+            'api:vehiculo:crear',
+            'api:vehiculo:listar',
+            'api:vehiculo:actualizar',
+            'api:vehiculo:eliminar',
+
+            'api:estado_solicitud:crear',
+            'api:estado_solicitud:listar',
+            'api:estado_solicitud:actualizar',
+            'api:estado_solicitud:eliminar',
+
+            'api:user:crear',
+            'api:user:listar',
+            'api:user:actualizar',
+            'api:user:eliminar',
+
+            'api:desplazamiento:crear',
+            'api:desplazamiento:listar',
+            'api:desplazamiento:actualizar',
+            'api:desplazamiento:eliminar',
+
+            'api:solicitud_cuenta:crear',
+            'api:solicitud_cuenta:listar',
+            'api:solicitud_cuenta:actualizar',
+            'api:solicitud_cuenta:eliminar',
+
+            'api:reporte_incidente:crear',
+            'api:reporte_incidente:listar',
+            'api:reporte_incidente:actualizar',
+            'api:reporte_incidente:eliminar',
+
+            'api:detalle_medio_recorrido',
+            'api:detalle_medio_recorrido',
+            'api:detalle_medio_recorrido',
+            'api:detalle_medio_recorrido',
+
+            'api:tipos_vehiculos_rutas:crear',
+            'api:tipos_vehiculos_rutas:listar',
+            'api:tipos_vehiculos_rutas:actualizar',
+            'api:tipos_vehiculos_rutas:eliminar',
+
+            'api:tipos_servicios_rutas:crear',
+            'api:tipos_servicios_rutas:listar',
+            'api:tipos_servicios_rutas:actualizar',
+            'api:tipos_servicios_rutas:eliminar',
+
+            'api:clases_servicios_rutas:crear',
+            'api:clases_servicios_rutas:listar',
+            'api:clases_servicios_rutas:actualizar',
+            'api:clases_servicios_rutas:eliminar',
+
+            'api:rutas_transportes:crear',
+            'api:rutas_transportes:listar',
+            'api:rutas_transportes:actualizar',
+            'api:rutas_transportes:eliminar',
+
+            'api:levantamiento:crear',
+            'api:levantamiento:listar',
+            'api:levantamiento:actualizar',
+            'api:levantamiento:eliminar',
+
+            'api:reporte_marcadores:crear',
+            'api:reporte_marcadores:listar',
+            'api:reporte_marcadores:actualizar',
+            'api:reporte_marcadores:eliminar',
+
+            'api:bitacora_tablas:crear',
+            'api:bitacora_tablas:listar',
+            'api:bitacora_tablas:actualizar',
+            'api:bitacora_tablas:eliminar',
+
+            'api:levantamiento_contador:crear',
+            'api:levantamiento_contador:listar',
+            'api:levantamiento_contador:actualizar',
+            'api:levantamiento_contador:eliminar',
+
+            'api:reporte_contador:crear',
+            'api:reporte_contador:listar',
+            'api:reporte_contador:actualizar',
+            'api:reporte_contador:eliminar',
+
+            'api:terminos_condiciones:crear',
+            'api:terminos_condiciones:listar',
+            'api:terminos_condiciones:actualizar',
+            'api:terminos_condiciones:eliminar',
         ]);
 
         $sitios = collect([
@@ -84,7 +193,7 @@ class RolPermissionSeeder extends Seeder
         $roleAdministrador = Role::create(['name' => Constant::ROL_ADMINISTRADOR]);
         $roleInvestigador = Role::create(['name' => Constant::ROL_INVESTIGADOR]);
         $roleParticipante = Role::create(['name' => Constant::ROL_PARTICIPANTE]);
-        
+
         $sitiosInvestigador = [
             'web:desplazamiento:movil',
             'web:desplazamiento:detalle',
@@ -103,7 +212,7 @@ class RolPermissionSeeder extends Seeder
             'web:desplazamiento:detalle',
         ];
 
-        foreach ($sitios as $sitio){
+        foreach ($sitios as $sitio) {
             $permission = Permission::create([
                 'name' => $sitio,
                 'guard_name' => 'web'
@@ -124,18 +233,25 @@ class RolPermissionSeeder extends Seeder
         }
 
         foreach ($modelos as $modelo) {
-            foreach ($accionesDisponibles as $accion) {
-                $permission = Permission::create([
-                    'name' => $guardApi . $separador . $modelo . $separador . $accion,
-                    'guard_name' => 'web'
-                ]);
 
-                $roleAdministrador->givePermissionTo($permission);
+            $permission = Permission::create([
+                'name' => $modelo,
+                'guard_name' => 'web'
+            ]);
 
-                if (in_array($accion, ['listar', 'crear'])) {
-                    $roleParticipante->givePermissionTo($permission);
-                    $roleInvestigador->givePermissionTo($permission);
-                }
+            $roleAdministrador->givePermissionTo($permission);
+
+            if (Str::startsWith($permission, '110')) {
+                $roleInvestigador->givePermissionTo($permission);
+            }
+
+            if (Str::startsWith($permission, '111')) {
+                $roleInvestigador->givePermissionTo($permission);
+            }
+
+
+            if (in_array($accion, ['listar', 'crear'])) {
+                $roleParticipante->givePermissionTo($permission);
             }
         }
     }
