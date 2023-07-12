dd4340c00fcd083a22590c5556eb762ca52b53ea
kernel503 <33852796+kernel503@users.noreply.github.com>
2023-07-12 08:49:14 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 sudo chown -R $USER:www-data storage
 sudo chown -R $USER:www-data bootstrap/cache
-chmod -R 775 storage
-chmod -R 775 bootstrap/cache
+sudo chmod -R 775 storage
+sudo chmod -R 775 bootstrap/cache
 docker compose run --user root php composer install
 docker compose run php php artisan migrate:fresh --seed
 docker compose exec -u root php php artisan key:generate
